68cad7016bb860a4b0380321a5f65e372d8af546
Samuel Liard <samuel.liard@babbar.tech>
2023-02-17 07:52:08 +0000

    test dream model

--- scripts/txt2img.py
+++ scripts/txt2img.py
@@ -51,9 +51,12 @@ def load_model_from_config(config, ckpt, verbose=False):
     if "global_step" in pl_sd:
         print(f"Global Step: {pl_sd['global_step']}")
 
-    print(pl_sd.keys())
+    # print(pl_sd.keys())
+    if "state_dict" in pl_sd:
+        sd = pl_sd["state_dict"]
+    else:
+        sd = pl_sd
 
-    sd = pl_sd["state_dict"]
     model = instantiate_from_config(config.model)
     m, u = model.load_state_dict(sd, strict=False)
     if len(m) > 0 and verbose:
