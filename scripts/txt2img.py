87c56c36598d575b0bb51bd0f42a0d3b022d34bb
Samuel Liard <samuel.liard@babbar.tech>
2023-02-17 08:41:11 +0000

    fix for dreamshaper

--- scripts/txt2img.py
+++ scripts/txt2img.py
@@ -50,7 +50,13 @@ def load_model_from_config(config, ckpt, verbose=False):
     pl_sd = torch.load(ckpt, map_location="cpu")
     if "global_step" in pl_sd:
         print(f"Global Step: {pl_sd['global_step']}")
-    sd = pl_sd["state_dict"]
+
+    #print(pl_sd.keys())
+    if "state_dict" in pl_sd:
+        sd = pl_sd["state_dict"]
+    else:
+        sd = pl_sd
+
     model = instantiate_from_config(config.model)
     m, u = model.load_state_dict(sd, strict=False)
     if len(m) > 0 and verbose:
