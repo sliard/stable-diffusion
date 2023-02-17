b6f6a632604023d00ced48c90707bb0ed89df1c4
Samuel Liard <samuel.liard@babbar.tech>
2023-02-17 07:41:23 +0000

    test dream model

--- scripts/txt2img.py
+++ scripts/txt2img.py
@@ -50,6 +50,9 @@ def load_model_from_config(config, ckpt, verbose=False):
     pl_sd = torch.load(ckpt, map_location="cpu")
     if "global_step" in pl_sd:
         print(f"Global Step: {pl_sd['global_step']}")
+
+    print(pl_sd.keys())
+
     sd = pl_sd["state_dict"]
     model = instantiate_from_config(config.model)
     m, u = model.load_state_dict(sd, strict=False)
